8102b9d6d799ec69865bfad099fffd85c1f3d956
sikarwar2010 <sikarwar2010@gmail.com>
2023-08-18 02:06:44 +0530

    contact page upload

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import Information from '@/components/Contact/Information';
+import React from 'react'
+
+const ContactUs = () => {
+    return (
+        <div>
+            <main>
+                <Information />
+            </main>
+        </div>
+    )
+}
+
+export default ContactUs;
